d6525857f3162bc43bda3c4b45c267f0b9b478bd
mdisoft126 <mdisoft126@gmail.com>
2022-01-06 00:31:27 +0100

    spring project initialization

---
+++ springMuslibry/src/main/java/com/marcind/SpringMuslibryApplication.java
@@ -0,0 +1,13 @@
+package com.marcind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMuslibryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMuslibryApplication.class, args);
+	}
+
+}
