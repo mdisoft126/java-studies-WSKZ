068d182a19ed7fb863e51e902b6da0b6b7fd8dd5
mdisoft126 <mdisoft126@gmail.com>
2022-01-09 00:28:47 +0100

    entities added

--- springMuslibry/src/main/java/com/marcind/model/Artist.java
+++ springMuslibry/src/main/java/com/marcind/model/Artist.java
@@ -3,11 +3,84 @@ package com.marcind.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Artist {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 	
 	private String firstName;
 	private String lastName;
 	private String nick;
 	
+	@ManyToMany(mappedBy = "artists")
 	private Set<Song> songs = new HashSet<>();
+	
+	// constructors
+	public Artist(String firstName, String lastName, String nick) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.nick = nick;
+	}
+	public Artist() {
+		super();
+	}
+
+	// getters and setters
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+		
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getNick() {
+		return nick;
+	}
+
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+
+	public Set<Song> getSongs() {
+		return songs;
+	}
+
+	public void setSongs(Set<Song> songs) {
+		this.songs = songs;
+	}
+	
+	// to string
+	@Override
+	public String toString() {
+		return "Artist [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", nick=" + nick
+				+ ", songs=" + songs + "]";
+	}
+	
 }
--- springMuslibry/src/main/java/com/marcind/model/Song.java
+++ springMuslibry/src/main/java/com/marcind/model/Song.java
@@ -3,7 +3,19 @@ package com.marcind.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Song {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private Long id;
 	
 	private String title;
 	private String genre;
@@ -11,12 +23,37 @@ public class Song {
 	private String year;
 	private String publisher;
 	
+	@ManyToMany
 	private Set<Artist> artists = new HashSet<>();
 
+	
+	// constructors
+	public Song(String title, String genre, String ismn, String year, String publisher) {
+		super();
+		this.title = title;
+		this.genre = genre;
+		this.ismn = ismn;
+		this.year = year;
+		this.publisher = publisher;
+	}
+	public Song() {
+		super();
+	}
+
+	// getter and setters
+	
 	public String getTitle() {
 		return title;
 	}
 
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
@@ -61,5 +98,11 @@ public class Song {
 		this.artists = artists;
 	}
 	
+	// to string
+	@Override
+	public String toString() {
+		return "Song [id=" + id + ", title=" + title + ", genre=" + genre + ", ismn=" + ismn + ", year=" + year
+				+ ", publisher=" + publisher + ", artists=" + artists + "]";
+	}
 
 }
