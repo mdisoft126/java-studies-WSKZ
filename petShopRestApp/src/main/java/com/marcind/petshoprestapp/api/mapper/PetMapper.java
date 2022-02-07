eb2d8b511e1014c5611510b3fae2e7b2099834ef
mdisoft126 <mdisoft126@gmail.com>
2022-02-08 00:35:02 +0100

    mapstruct generated

---
+++ petShopRestApp/src/main/java/com/marcind/petshoprestapp/api/mapper/PetMapper.java
@@ -0,0 +1,16 @@
+package com.marcind.petshoprestapp.api.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import com.marcind.petshoprestapp.api.model.PetDTO;
+import com.marcind.petshoprestapp.domain.Pet;
+
+@Mapper
+public interface PetMapper {
+	
+	PetMapper INSTANCE = Mappers.getMapper(PetMapper.class);
+	
+	PetDTO petToPetDTO(Pet pet);
+
+}
