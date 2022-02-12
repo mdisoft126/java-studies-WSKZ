516de6935abd1b0bc8bc075ab3e7e1aeee9ebf07
mdisoft126 <mdisoft126@gmail.com>
2022-02-13 00:44:26 +0100

    collectionResourceRel added

--- springDataRestRepositories/src/main/java/com/marcind/springdatarestrepositories/repositories/PetRepository.java
+++ springDataRestRepositories/src/main/java/com/marcind/springdatarestrepositories/repositories/PetRepository.java
@@ -3,8 +3,11 @@ package com.marcind.springdatarestrepositories.repositories;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 import com.marcind.springdatarestrepositories.domain.Pet;
 
+@RepositoryRestResource(collectionResourceRel = "pet", path = "pet")
 public interface PetRepository extends JpaRepository<Pet, Long> {
 	
 	List<Pet> findByStatus(@Param("status") String status);
