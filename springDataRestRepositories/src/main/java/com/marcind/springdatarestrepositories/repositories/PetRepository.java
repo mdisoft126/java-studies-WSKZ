83b2fcf73173125125f876ddb79c37dca9c915ef
mdisoft126 <mdisoft126@gmail.com>
2022-02-13 00:34:34 +0100

    hateoas approach implemented

--- springDataRestRepositories/src/main/java/com/marcind/springdatarestrepositories/repositories/PetRepository.java
+++ springDataRestRepositories/src/main/java/com/marcind/springdatarestrepositories/repositories/PetRepository.java
@@ -1,8 +1,12 @@
 package com.marcind.springdatarestrepositories.repositories;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import com.marcind.springdatarestrepositories.domain.Pet;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
+	
+	List<Pet> findByStatus(@Param("status") String status);
 
 }
