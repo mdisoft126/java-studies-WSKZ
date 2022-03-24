81c7e3ed761ae15c2b0584302853e6a581b18c54
mdisoft126 <mdisoft126@gmail.com>
2022-03-24 23:37:10 +0100

    Rest API finished

--- restApi/src/main/java/com/marcind/restapi/controller/AppController.java
+++ restApi/src/main/java/com/marcind/restapi/controller/AppController.java
@@ -1,14 +1,47 @@
 package com.marcind.restapi.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.marcind.restapi.model.dto.NameDto;
 
 @RestController
 public class AppController {
 	
 	@GetMapping("hello")
-	public String hello() {
-		return "Hello world!";
+	public ResponseEntity<String> hello() {
+		return new ResponseEntity<>("Hello World", HttpStatus.OK);
+	}
+	
+	@GetMapping("hello/{name}")
+	public String helloName(@PathVariable String name) {
+		return "Hello " + name + "!";
+	}
+	
+	@GetMapping("hello/param")
+	public String helloParam(@RequestParam String name) {
+		return "Hello " + name + "!";
+	}
+	
+	@PostMapping("hello/body")
+	public ResponseEntity<String> helloBody(@RequestBody NameDto nameDto) {
+		if(nameDto.getName().equals("Marcin")) {
+			return new ResponseEntity<>("OK", HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>("NOT FOUND", HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@GetMapping("hello/header")
+	public String helloHeader(@RequestHeader("name") String name) {
+		return "Hello " + name + "!";
 	}
 
 }
---
+++ restApi/src/main/java/com/marcind/restapi/model/dto/NameDto.java
@@ -0,0 +1,16 @@
+package com.marcind.restapi.model.dto;
+
+public class NameDto {
+
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+}
